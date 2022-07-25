df7889fa616bd79e873c0081372c7b6a2f664802
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-07-25 16:43:40 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let yr = new Date().getFullYear()
+
+const date = document.getElementById('date');
+
+date.innerHTML = yr + ".";
+
+
+/*let toggle = document.querySelector(".toggle")
+let nav =document.getElementsByTagName("nav")
+
+let checkToggle = () =>{
+    let tog = toggle.classList.contains("#navbar")
+    if(tog){
+        toggle.classList.remove("#navbar")
+    }else{
+        toggle.classList.add("#navbar")
+    
+    }
+}
+checkToggle() */
